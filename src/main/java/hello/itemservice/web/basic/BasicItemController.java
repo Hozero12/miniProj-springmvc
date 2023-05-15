4ec3c3bb15ba02403155832498c5149408eefb63
amb258012@gmail.com <amb258012@gmail.com>
2023-05-15 18:05:01 +0900

    상품추가시 PRG 적용, redirectAttributes 적용

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -60,11 +61,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+   // @PostMapping("/add")
     public String addItemV3(Item item ){
         itemRepository.save(item);
         return "basic/item";
     }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";  //PRG 적용
+        // return "basic/item";
+    }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model){
